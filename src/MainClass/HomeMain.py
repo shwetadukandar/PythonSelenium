24d3260ef58d3a767206e5bff7df86b3fe67f2f5
Gouri <Gouri@Gouri-PC>
2017-03-29 00:48:45 -0700

    HomePage updated

---
+++ src/MainClass/HomeMain.py
@@ -0,0 +1,16 @@
+'''
+Created on Mar 24, 2017
+
+@author: Gouri
+'''
+from PageClass.DepartmentPage import DepartmentPage
+from PageClass.HomePage import HomePage
+import time
+
+if __name__ == '__main__':
+    d1=DepartmentPage()
+    d1.FirefoxPage()
+    
+    #time.sleep(4)
+   # h1=HomePage()
+    #h1.YourAmazoncomLinkClick()
